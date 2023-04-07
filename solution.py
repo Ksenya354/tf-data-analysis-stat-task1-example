4749950a92f1014f00bbb98591c81c753e127f78
Ksenya354 <123981850+Ksenya354@users.noreply.github.com>
2023-04-07 12:08:48 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1943224240 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     y = np.log(x)
