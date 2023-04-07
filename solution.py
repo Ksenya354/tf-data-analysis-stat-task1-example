019849031c7957b8738a2c7363c923e14272209a
Ksenya354 <123981850+Ksenya354@users.noreply.github.com>
2023-04-07 12:03:12 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
+
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    y = np.log(x)
+    m = y.mean()
+    sd = np.std(y)
+    a = np.exp(m+(sd**2)/2)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return a # Ваш ответ
