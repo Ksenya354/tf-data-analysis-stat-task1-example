cbff601fd341d4ea017ad8ffaf44887c0ded5380
Ksenya354 <123981850+Ksenya354@users.noreply.github.com>
2023-04-07 17:53:38 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,8 @@ chat_id = 1943224240 # Ваш chat ID, не меняйте название пе
 
 def solution(x: np.array) -> float:
     y = np.log(x)
-    m = 247+y.mean()
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return m # Ваш ответ
+    m = 247 + y.mean()
+    sd = np.std(y)
+    a = np.exp(m+(sd**2)/2)
+    sigma = np.exp(2*m + sd**2)*(np.exp(sd**2)-1)
+    return m
