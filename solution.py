61530df53e076e13ca8e60202ce414571444f220
Ksenya354 <123981850+Ksenya354@users.noreply.github.com>
2023-04-12 16:22:50 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,6 @@ import numpy as np
 chat_id = 1943224240 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    y = 247 + np.log(x)
+    y = np.log(x-247)
     m = y.mean()
-    sd = np.std(y)
-    a = np.exp(m+(sd**2)/2)
     return m
